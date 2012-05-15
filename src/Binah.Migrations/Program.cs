48ecf924b8fb60b8c9031eb7e36f0ee33e5057ee
Fitzchak Yitzchaki <fitzchak@ayende.com>
2012-05-15 17:52:52 +0300

    Use UTF8 encoding

--- src/Binah.Migrations/Program.cs
+++ src/Binah.Migrations/Program.cs
@@ -1,9 +1,15 @@
-﻿namespace Binah.Migrations
+﻿using System;
+using System.Text;
+
+namespace Binah.Migrations
 {
 	internal class Program
 	{
 		private static void Main(string[] args)
 		{
+			Console.InputEncoding = Encoding.UTF8;
+			Console.OutputEncoding = Encoding.UTF8;
+
 			new CommandsRunner().LookForCommands();
 		}
 	}
