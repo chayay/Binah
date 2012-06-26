8222845e5a71b3590106e972210ffc566896166c
Fitzchak Yitzchaki <fitzchak@ayende.com>
2012-06-26 15:52:18 +0300

    Add CGJ char

--- src/Binah.Core/Hebrew/YiddishDigraph.cs
+++ src/Binah.Core/Hebrew/YiddishDigraph.cs
@@ -8,7 +8,13 @@ namespace Binah.Core.Hebrew
 
 		public const char AlefLamed = '\uFB4F';
 		public const char ZeroWidthJoiner = '\u200D';
+		public const char CombiningGraphemeJoiner = '\u034F';
 
 		public const char NunHafukha = '\u05C6';
+		public const char YodWithHiriq = '\u05C6';
+
+		public const char RingAbove = '\u030A';
+		public const char DotAbove = '\u0307';
+		public const char BlackCircle = '\u2022';
 	}
 }
