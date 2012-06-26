b00aaa3cc3b94450be6d2090b332e256bea24b38
Fitzchak Yitzchaki <fitzchak@ayende.com>
2012-06-26 15:52:54 +0300

    Add some snippets IDs

--- src/Binah.Siddur/TefillahResolvers/ShacharitResolver.cs
+++ src/Binah.Siddur/TefillahResolvers/ShacharitResolver.cs
@@ -2,6 +2,18 @@
 {
 	public class ShacharitResolver
 	{
-		 
+		public ShacharitResolver()
+		{
+			var snippets = new[]
+			{
+				"Hodu",
+				"Mizmor-Shir-Chanukat-HaBayit-LDavid",
+				"Hashem-Melech",
+				"Vehaya-Hashem-LMelech",
+				"Hoshienu-Hashem-Elokeinu",
+				"Lamnatzeach-Binginot",
+				"LeShem-Yichud",
+			};
+		}
 	}
 }
