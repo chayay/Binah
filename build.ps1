0c55388eaad09193f8a69b9a673460f98d859e9c
Fitzchak Yitzchaki <fitzchak@ayende.com>
2012-06-28 15:03:16 +0300

    Add build files

---
+++ build.ps1
@@ -0,0 +1,7 @@
+﻿param($task = "default")
+
+get-module psake | remove-module
+
+import-module (Get-ChildItem .\src\packages\psake.*\tools\psake.psm1 | Select-Object -First 1)
+
+invoke-psake .\tasks.ps1 $task
---
+++ tasks.ps1
@@ -0,0 +1,35 @@
+﻿properties {
+	$base_dir  = resolve-path .
+	$bin_dir = "$base_dir\bin"
+}
+
+Task default -Depends Test
+
+Task Clean {
+   "clean"
+}
+
+Task Init -depends Clean {
+   "init"
+}
+
+Task Compile -Depends Init {
+   "compile"
+}
+
+Task Test -Depends Compile {
+	"This is a test"
+}
+
+Task Release {
+	
+}
+
+Task ExportData {
+	$dump_dir = "$bin_dir\DataDumps"
+	$dump_dir
+	New-Item $dump_dir -Type Directory -ErrorAction SilentlyContinue | Out-Null
+	
+	$smuggler = "C:\RavenDB\Smuggler\Raven.Smuggler.exe"
+	. $smuggler out http://localhost:8080/ "$dump_dir\Documents.dump.zip" --database=Binah --operate-on-types=Documents
+}
