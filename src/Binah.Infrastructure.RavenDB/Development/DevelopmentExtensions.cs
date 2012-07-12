680fe56410c89fdfc0ea2504ddac6555c87e8bf4
Fitzchak Yitzchaki <fitzchak@ayende.com>
2012-07-12 17:17:30 +0300

    Add utility class to refactor ravendb entities

---
+++ src/Binah.Infrastructure.RavenDB/Development/DevelopmentExtensions.cs
@@ -0,0 +1,45 @@
+﻿using System.Linq;
+using Raven.Client;
+
+namespace Binah.Infrastructure.RavenDB.Development
+{
+	public static class DevelopmentExtensions
+	{
+		 public static DevelopmentRefactoring Development(this IDocumentStore store)
+		 {
+			 return new DevelopmentRefactoring(store);
+		 }
+	}
+
+	public class DevelopmentRefactoring
+	{
+		private readonly IDocumentStore store;
+
+		public DevelopmentRefactoring(IDocumentStore store)
+		{
+			this.store = store;
+		}
+
+		public void LoadAndSave<T>()
+		{
+			using (var session = store.OpenSession())
+			{
+				int skip = 0;
+				const int take = 1024;
+				while (true)
+				{
+					var builds = session.Query<T>()
+						.Skip(skip)
+						.Take(take)
+						.ToList();
+					skip += builds.Count;
+
+					session.SaveChanges();
+
+					if (builds.Count == 0)
+						break;
+				}
+			}
+		}
+	}
+}
---
+++ src/Binah.Tests.Unit/Infrastructure/RavenDB/DevelopmentExtensionsTests.cs
@@ -0,0 +1,33 @@
+﻿using Binah.Infrastructure.RavenDB.Development;
+using Raven.Client;
+using Raven.Client.Embedded;
+using Xunit;
+
+namespace Binah.Tests.Unit.Infrastructure.RavenDB
+{
+	public class DevelopmentExtensionsTests
+	{
+		private readonly IDocumentStore store;
+
+		public DevelopmentExtensionsTests()
+		{
+			store = new EmbeddableDocumentStore { RunInMemory = true }.Initialize();
+			using (var session = store.OpenSession())
+			{
+				session.Store(new Item {Id = "item/1"});
+				session.SaveChanges();
+			}
+		}
+
+		[Fact]
+		public void CanRenameTheIdOfEntity()
+		{
+			store.Development();
+		}
+
+		private class Item
+		{
+			public string Id { get; set; }
+		}
+	}
+}
