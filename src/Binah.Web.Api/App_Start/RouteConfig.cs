94b0858d46906396b17d6063d1fac3137081278d
Fitzchak Yitzchaki <fitzchak@ayende.com>
2012-07-17 21:04:36 +0300

    Merge routing config

--- src/Binah.Web.Api/App_Start/RouteConfig.cs
+++ src/Binah.Web.Api/App_Start/RouteConfig.cs
@@ -1,6 +1,8 @@
 ﻿using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Routing;
+using AttributeRouting.Web.Http.WebHost;
+using Binah.Web.Api.Controllers;
 
 namespace Binah.Web.Api
 {
@@ -10,7 +12,7 @@ namespace Binah.Web.Api
 		{
 			routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-			AttributeRoutingHttp.RegisterRoutes(routes);
+			RegisterRoutesUsingAttributeRouting(routes);
 
 			routes.MapHttpRoute(
 				name: "DefaultApi",
@@ -23,6 +25,18 @@ namespace Binah.Web.Api
 				url: "{controller}/{action}/{id}",
 				defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
 			);*/
+		}
+
+		private static void RegisterRoutesUsingAttributeRouting(RouteCollection routes)
+		{
+			// See http://github.com/mccalltd/AttributeRouting/wiki for more options.
+			// To debug routes locally using the built in ASP.NET development server, go to /routes.axd
+
+			// ASP.NET Web API
+			routes.MapHttpAttributeRoutes(configuration =>
+			{
+				configuration.ScanAssemblyOf<AbstractApiController>();
+			});
 		}
 	}
 }
