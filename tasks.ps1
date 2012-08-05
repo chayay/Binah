0497335fc437570fc4c1f090736687473f6ddc32
Fitzchak Yitzchaki <fitzchak@ayende.com>
2012-08-05 20:09:35 +0300

    Add task to run RavenDB from the build script

--- tasks.ps1
+++ tasks.ps1
@@ -1,6 +1,12 @@
-﻿properties {
+﻿Include ".\build_utils.ps1"
+
+properties {
 	$base_dir  = resolve-path .
 	$bin_dir = "$base_dir\bin"
+	$src_dir = "$base_dir\src"
+	$config_dir = "$base_dir\config"
+	$packages_dir = "$base_dir\src\packages"
+	$nuget_exe = "$src_dir\.nuget\NuGet.exe"
 }
 
 Task default -Depends Test
@@ -23,6 +29,18 @@ Task Test -Depends Compile {
 
 Task Release {
 	
+}
+
+Task RunDB {
+	$ravenServerPackageFolder = Get-ChildItem "$packages_dir\RavenDB.Server.*" | 
+										Sort-Object Name -Descending | 
+										Select-Object -First 1
+										
+	$ravenServerFolder = "$ravenServerPackageFolder\tools"
+	
+	Copy-Item "$config_dir\RavenDB-DeveloperMachine.config" "$ravenServerFolder\Raven.Server.exe.config"
+	
+	$developerRavenProcess = Start-Process "$ravenServerFolder\Raven.Server.exe" "--config=$config_dir\RavenDB-DeveloperMachine.config" -PassThru
 }
 
 Task ExportData {
