85f9a546282c8b30350df7cf1f2168e16a4514f2
Fitzchak Yitzchaki <fitzchak@ayende.com>
2012-08-28 12:23:04 +0300

    Add a passing test: DoesNotContainsANotRecognizedChar. Add a failing test: ShouldContain67Words. Failure reason: expecting 67 words but found 64 words.

--- src/Binah.Tests.Unit/Library/Siddur/Importers/PrayerForTravelersTests.cs
+++ src/Binah.Tests.Unit/Library/Siddur/Importers/PrayerForTravelersTests.cs
@@ -1,7 +1,9 @@
-﻿using Binah.Core.Models;
+﻿using System;
+using Binah.Core.Hebrew;
+using Binah.Core.Models;
 using Binah.Siddur.Import;
 using Xunit;
-using Binah.Core.Hebrew;
+using Binah.Core.Extensions;
 
 namespace Binah.Tests.Unit.Library.Siddur.Importers
 {
@@ -12,6 +14,26 @@ namespace Binah.Tests.Unit.Library.Siddur.Importers
 		{
 			var snippet = GetSnippet();
 			Assert.NotNull(snippet);
+		}
+
+		[Fact]
+		public void ShouldContain67Words()
+		{
+			var content = GetSnippet().Content;
+			Assert.NotNull(content);
+			var words = content.Split(new[]{' '});
+			words.ForEach(Console.WriteLine);
+			Assert.Equal(67, words.Length);
+		}
+
+		[Fact]
+		public void DoesNotContainsANotRecognizedChar()
+		{
+			var content = GetSnippet().Content;
+			foreach (var c in content)
+			{
+				Assert.True(c == ' ' || c == ':' || c.IsHebrewLetter() || c.IsVowel(), string.Format(@"'{0}' is not a recognized char. Unicode: \u{1:X4}.", c, (int) c));
+			}
 		}
 
 		private SiddurSnippet GetSnippet()
