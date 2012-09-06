d9f96c3d6ef77559b6893a6a963bb9d61db17f3a
Fitzchak Yitzchaki <fitzchak@ayende.com>
2012-09-06 13:07:18 +0300

    minor

--- src/Binah.Core/Models/User.cs
+++ src/Binah.Core/Models/User.cs
@@ -22,4 +22,15 @@
 	{
 		public string Language { get; set; }
 	}
+
+	public class UserAddress
+	{
+		public string Address1 { get; set; }
+		public string Address2 { get; set; }
+		public string City { get; set; }
+		public string State { get; set; }
+		public string Zipcode { get; set; }
+		public float Latitude { get; set; }
+		public float Longitude { get; set; }
+	}
 }
