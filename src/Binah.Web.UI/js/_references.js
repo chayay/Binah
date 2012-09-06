4eea5b86ef1e0b3ed2bb63f99715df031ef48c95
Fitzchak Yitzchaki <fitzchak@ayende.com>
2012-09-06 19:53:46 +0300

    add guidance links for CSS and JS conventions

--- src/Binah.Web.UI/js/_references.js
+++ src/Binah.Web.UI/js/_references.js
@@ -1,4 +1,6 @@
-﻿/// <reference path="libs/angular/angular.js" />
+﻿// https://github.com/rwldrn/idiomatic.js
+
+/// <reference path="libs/angular/angular.js" />
 /// <reference path="libs/angular/angular-resource.js" />
 /// <reference path="libs/angular/angular-sanitize.js" />
 /// <reference path="app.js" />
