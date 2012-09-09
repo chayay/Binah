cda774f8c15f5b863e0755c56b46d0efaf0e9364
Fitzchak Yitzchaki <fitzchak@ayende.com>
2012-09-09 21:20:10 +0300

    Wrap each word in a span tag. This: 1. fix the issue with blank space between letters in a word. 2. This lets styling a specific span.

--- src/Binah.Web.UI/js/filters.js
+++ src/Binah.Web.UI/js/filters.js
@@ -2,13 +2,14 @@
 
 /* Filters */
 
-angular.module('BinahApp.filters', []);
-
-/*
 angular.module('BinahApp.filters', []).
-  filter('interpolate', ['version', function(version) {
-    return function(text) {
-      return String(text).replace(/\%VERSION\%/mg, version);
-    }
-  }]);
-  */
+    filter('spanWords', function () {
+        return function (text) {
+            var result = text.split(' ')
+                .map(function(value, index, array) {
+                    return '<span index="' + index + '">' + value + '</span>';
+                })
+                .join(' ');
+            return result;
+        };
+    });
