b34cf1ca2c026dfc6ab76431d303ae3d6e49e55d
Fitzchak Yitzchaki <fitzchak@ayende.com>
2012-09-21 14:31:56 +0300

    Add exception planned to be used

---
+++ src/Binah.Tests.Unit/Library/Unicode/InvalidHebrewUnicodeOrderException.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Runtime.Serialization;
+using System.Security;
+
+namespace Binah.Tests.Unit.Library.Unicode
+{
+	[Serializable]
+	public class InvalidHebrewUnicodeOrderException : Exception
+	{
+		public char[] InvalidPattern { get; set; }
+		public char[] CorrectPattern { get; set; }
+
+		public InvalidHebrewUnicodeOrderException()
+		{
+		}
+
+		public InvalidHebrewUnicodeOrderException(string message) : base(message)
+		{
+		}
+
+		public InvalidHebrewUnicodeOrderException(string message, Exception inner) : base(message, inner)
+		{
+		}
+
+		// This constructor is needed for serialization.
+		[SecuritySafeCritical]  // auto-generated
+		protected InvalidHebrewUnicodeOrderException(SerializationInfo info, StreamingContext context) : base(info, context)
+		{
+		}
+
+		public InvalidHebrewUnicodeOrderException(char[] invalidPattern, char[] correctPattern)
+		{
+			InvalidPattern = invalidPattern;
+			CorrectPattern = correctPattern;
+		}
+	}
+}
